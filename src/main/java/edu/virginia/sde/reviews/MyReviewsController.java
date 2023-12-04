8cbae5d6bbd59a56ef9d61a69093a7118c0b0f61
Pravallika Kullampalle <pkullampalle@gmail.com>
2023-12-04 15:39:10 -0500

    Worked on MyReviews

--- src/main/java/edu/virginia/sde/reviews/MyReviewsController.java
+++ src/main/java/edu/virginia/sde/reviews/MyReviewsController.java
@@ -2,12 +2,18 @@ package edu.virginia.sde.reviews;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -64,5 +70,16 @@ public class MyReviewsController {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+
+    }
+
+    @FXML
+    private void goBack(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("course-search-scene.fxml"));
+        Parent secondPage = loader.load();
+        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(secondPage));
+        stage.show();
     }
 }
